84363966522311ad3c071d69bbe9a15335a2d199
MosecX <111986841+MosecX@users.noreply.github.com>
2024-05-18 15:11:17 -0600

    Update systemctl.sh

--- systemctl.sh
+++ systemctl.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-echo "Actualizano paquetes"
+BOLD_GREEN='\e[1;32m'
+
+RESET_COLOR='\e[0m'
+
+echo "${BOLD_GREEN}Actualizano paquetes"
 apt update
 apt install -y curl
-echo "Activando SYSTEMCTL"
-curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py
+echo "${BOLD_GREEN}Activando SYSTEMCTL ${RESET_COLOR}"
+curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/mast>
 chmod +x /bin/systemctl
