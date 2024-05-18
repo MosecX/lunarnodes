c83c920020bf6dec6c0725d7ac9d794ca62ffc28
MosecX <111986841+MosecX@users.noreply.github.com>
2024-05-18 15:04:45 -0600

    Update systemctl.sh

--- systemctl.sh
+++ systemctl.sh
@@ -1,1 +1,8 @@
+#!/bin/sh
 
+echo "Actualizano paquetes"
+apt update
+apt install -y curl
+echo "Activando SYSTEMCTL"
+curl -o /bin/systemctl https://raw.githubusercontent.com/gdraheim/docker-systemctl-replacement/master/files/docker/systemctl3.py
+chmod +x /bin/systemctl
